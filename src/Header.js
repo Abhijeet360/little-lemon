b7582adc88b622f3a3e7863c3bf771c30b4ed7bf
Abhijeet Pandey <abhijeetpandey360@gmail.com>
2023-02-12 23:16:26 +0530

    add assets and add logo and navbar

---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+const Header = () => {
+
+    return (
+        <>
+            <header> <img src='assets/Logo.svg' alt="error" /></header>
+            <nav>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#About">About</a></li>
+                    <li><a href="#menu">Menu</a></li>
+                    <li><a href="#reservations">reservations</a></li>
+                    <li><a href="#orderonline">order online</a></li>
+                    <li><a href="#login">login</a></li>
+                </ul>
+            </nav>
+
+        </>
+    )
+}
+
+export default Header;
