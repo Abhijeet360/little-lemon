80a977f7a3824eefd8f3bf5511deedf3ca111b2e
Abhijeet Pandey <abhijeetpandey360@gmail.com>
2023-02-15 19:29:58 +0530

    Add style.css file and style navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from './Header';
+import Nav from './Nav';
+import './style.css';
 const App = () => {
   return (
-    <>
+    <div class="container">
       <Header />
-    </>
+      <Nav />
+    </div>
   )
 }
 
--- src/Header.js
+++ src/Header.js
@@ -4,17 +4,6 @@ const Header = () => {
     return (
         <>
             <header> <img src='assets/Logo.svg' alt="error" /></header>
-            <nav>
-                <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#About">About</a></li>
-                    <li><a href="#menu">Menu</a></li>
-                    <li><a href="#reservations">reservations</a></li>
-                    <li><a href="#orderonline">order online</a></li>
-                    <li><a href="#login">login</a></li>
-                </ul>
-            </nav>
-
         </>
     )
 }
